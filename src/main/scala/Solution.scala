efe7b42ff4df39124121b6ace741438777e537d6
lastseenjustnow <lastseenjustnow@protonmail.com>
2022-03-02 16:35:25 +0400

    LeetCode: Maximum Score from Performing Multiplication Operations (test)

--- src/main/scala/Solution.scala
+++ src/main/scala/Solution.scala
@@ -173,4 +173,23 @@ object Solution {
     rob(sums)
   }
 
+  def maximumScoreRecursive(nums: Array[Int], multipliers: Array[Int]): Int = {
+    /**
+     * You are given two integer arrays nums and multipliers of size n and m respectively, where n >= m. The arrays are 1-indexed.
+     *
+     * You begin with a score of 0. You want to perform exactly m operations. On the ith operation (1-indexed), you will:
+     *
+     * - Choose one integer x from either the start or the end of the array nums.
+     * - Add multipliers[i] * x to your score.
+     * - Remove x from the array nums.
+     *
+     * Return the maximum score after performing m operations.
+     * */
+    ???
+  }
+
+  def maximumScore(nums: Array[Int], multipliers: Array[Int]): Int = {
+    ???
+  }
+
 }
