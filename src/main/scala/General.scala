cd7d7e01a9420d28d14a30ff0aa8428e2e8c54fc
lastseenjustnow <lastseenjustnow@protonmail.com>
2022-03-21 14:22:57 +0400

    LeetCode: Biweekly Contest #74
    Tasks resolved:
    Divide Array Into Equal Pairs
    
    Tasks attempted:
    Maximize Number of Subsequences in a String
    
    Important notes:
    Take constraints & data formats into considerations. In 'Maximize Number of Subsequences in a String' 168 / 172 tests passed as internal variables inside function used incorrect data format (Int instead of Long)

--- src/main/scala/General.scala
+++ src/main/scala/General.scala
@@ -1,4 +1,5 @@
-import scala.math.abs
+import scala.collection.mutable
+import scala.math.{abs, max}
 
 object General {
   def assignBikes(workers: Array[Array[Int]], bikes: Array[Array[Int]]): Array[Int] = {
@@ -27,4 +28,50 @@ object General {
     }
     workersAssigned
   }
+
+
+  def divideArray(nums: Array[Int]): Boolean = {
+    val st: mutable.Set[Int] = mutable.Set[Int]()
+    for (number <- nums) {
+      if (st.contains(number)) {
+        st -= number
+      } else {
+        st += number
+      }
+    }
+    if (st.isEmpty) true else false
+  }
+
+  def maximumSubsequenceCount(text: String, pattern: String): Long = {
+    var (bal: Long, left: Long, right: Long, maxLeftRight: Long) = (0L, 0L, 0L, 0L)
+    for (ch <- text) {
+      if (ch == pattern(0)) {
+        left += 1
+      }
+      if (ch == pattern(1)) {
+        bal += (if (pattern(0) != pattern(1)) left else left - 1)
+        right += 1
+      }
+      maxLeftRight = Math.max(left, right)
+    }
+    bal + maxLeftRight
+  }
+
+  def halveArray(nums: Array[Int]): Int = {
+    val numsDoubles = nums.map(_.toDouble).sum
+    var (initialSum, currentSum) = (numsDoubles, numsDoubles)
+    val stack: mutable.Queue[Double] = mutable.Queue()
+    val sortedNums: mutable.Queue[Int] = mutable.Queue() ++ nums.sorted(Ordering.Int.reverse)
+    var iterNum = 0
+
+    while (sortedNums.nonEmpty & currentSum > initialSum / 2) {
+      val elem: Double = if (stack.isEmpty || stack.head < sortedNums.head) sortedNums.dequeue() else stack.dequeue()
+      val halfSumElem = elem / 2
+      currentSum -= halfSumElem
+      stack.enqueue(halfSumElem)
+      iterNum += 1
+    }
+    iterNum
+  }
+
 }
--- src/test/scala/GeneralTest.scala
+++ src/test/scala/GeneralTest.scala
@@ -35,4 +35,67 @@ class GeneralTest extends FunSuite with Matchers {
       assignBikes(cond._1, cond._2) should equal(cond._3)
     }
   }
+
+  test("Divide Array Into Equal Pairs") {
+    val conditions: Array[(Array[Int], Boolean)] =
+      Array(
+        (Array(3, 2, 3, 2, 2, 2), true),
+        (Array(1, 2, 3, 4), false),
+        (Array(1), false),
+        (Array(0, 1, 0, 1), true)
+      )
+
+
+    for (cond <- conditions) {
+      assert(divideArray(cond._1) == cond._2)
+    }
+  }
+
+  test("Maximize Number of Subsequences in a String") {
+    val conditions: Array[(String, String, Long)] =
+      Array(
+        ("abdcdbc", "ac", 4),
+        ("abdcdbc", "ae", 1),
+        ("ababb", "ab", 8),
+        ("aabbb", "ab", 9),
+        ("aabb", "ab", 6),
+        ("bbaa", "ab", 2),
+        ("abab", "aa", 3),
+        ("a", "ab", 1),
+        ("a", "cb", 0),
+        ("zzzz", "zz", 10),
+        ("vnedkpkkyxelxqptfwuzcjhqmwagvrglkeivowvbjdoyydnjrqrqejoyptzoklaxcjxbrrfmpdxckfjzahparhpanwqfjrpbslsyiwbldnpjqishlsuagevjmiyktgofvnyncizswldwnngnkifmaxbmospdeslxirofgqouaapfgltgqxdhurxljcepdpndqqgfwkfiqrwuwxfamciyweehktaegynfumwnhrgrhcluenpnoieqdivznrjljcotysnlylyswvdlkgsvrotavnkifwmnvgagjykxgwaimavqsxuitknmbxppgzfwtjdvegapcplreokicxcsbdrsyfpustpxxssnouifkypwqrywprjlyddrggkcglbgcrbihgpxxosmejchmzkydhquevpschkpyulqxgduqkqgwnsowxrmgqbmltrltzqmmpjilpfxocflpkwithsjlljxdygfvstvwqsyxlkknmgpppupgjvfgmxnwmvrfuwcrsadomyddazlonjyjdeswwznkaeaasyvurpgyvjsiltiykwquesfjmuswjlrphsdthmuqkrhynmqnfqdlwnwesdmiiqvcpingbcgcsvqmsmskesrajqwmgtdoktreqssutpudfykriqhblntfabspbeddpdkownehqszbmddizdgtqmobirwbopmoqzwydnpqnvkwadajbecmajilzkfwjnpfyamudpppuxhlcngkign", "rr", 496)
+      )
+
+    for (cond <- conditions) {
+      assert(maximumSubsequenceCount(cond._1, cond._2) == cond._3)
+    }
+
+    val source = scala.io.Source.fromResource("maximizeNumberOfSubsequencesInAString.txt")
+    val testcase: String = source.getLines().map(_.split(",")).next().head
+
+    assert(maximumSubsequenceCount(testcase, "zz") == 5000050000L)
+  }
+
+  test("Minimum Operations to Halve Array Sum") {
+    val conditions: Array[(Array[Int], Int)] =
+      Array(
+        (Array(5, 19, 8, 1), 3),
+        (Array(3, 8, 20), 3),
+        (Array(32, 98, 23, 14, 67, 40, 26, 9, 96, 96, 91, 76, 4, 40, 42, 2, 31, 13, 16, 37, 62, 2, 27, 25, 100, 94, 14, 3, 48, 56, 64, 59, 33, 10, 74, 47, 73, 72, 89, 69, 15, 79, 22, 18, 53, 62, 20, 9, 76, 64), 36),
+        (Array(6, 58, 10, 84, 35, 8, 22, 64, 1, 78, 86, 71, 77), 9),
+        (Array(1), 1),
+        (Array(1, 2), 2)
+      )
+
+    for (cond <- conditions) {
+      assert(halveArray(cond._1) == cond._2)
+    }
+
+    val source = scala.io.Source.fromResource("minimumOperationsToHalveArraySum.txt")
+    val testcase: Array[Int] = source.getLines().map(_.split(",").map(_.trim.toInt)).next()
+
+    assert(halveArray(testcase) == 10020)
+  }
+
 }
