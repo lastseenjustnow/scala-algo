fa1ca1d273466b7145de7ae5ccb9455ff5b20505
lastseenjustnow <lastseenjustnow@protonmail.com>
2022-03-30 16:48:25 +0400

    LeetCode: Count Unique Characters of All Substrings of a Given String
    Doesn't work - fails with Time Limit Exceeded.

--- src/main/scala/DynamicProgramming.scala
+++ src/main/scala/DynamicProgramming.scala
@@ -888,4 +888,25 @@ object DynamicProgramming {
     dp(k, 1, 1)
   }
 
+  def uniqueLetterStringFP(s: String): Int = {
+    /**
+     * Let's define a function countUniqueChars(s) that returns the number of unique characters on s.
+     *
+     * For example, calling countUniqueChars(s) if s = "LEETCODE" then "L", "T", "C", "O", "D" are the unique characters since they appear only once in s, therefore countUniqueChars(s) = 5.
+     * Given a string s, return the sum of countUniqueChars(t) where t is a substring of s.
+     *
+     * Notice that some substrings can be repeated so in this case you have to count the repeated ones too.
+     * */
+
+    def countUniqueChars(s: String): Int =
+      s
+        .groupBy(identity)
+        .mapValues(_.length)
+        .count({ case (_, occurence) => occurence == 1 })
+
+    (for (x <- 0 until s.length; y <- x until s.length) yield (x, y))
+      .foldLeft(0) { (x, indices) => x + countUniqueChars(s.slice(indices._1, indices._2 + 1)) }
+
+  }
+
 }
