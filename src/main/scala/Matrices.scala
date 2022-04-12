541435d8e08a28e19347ff1b83733b7e022b071f
lastseenjustnow <lastseenjustnow@protonmail.com>
2022-04-12 14:58:01 +0400

    LeetCode: Game of Life Constant space solution (in-place reassignments)

--- src/main/scala/Matrices.scala
+++ src/main/scala/Matrices.scala
@@ -1,5 +1,5 @@
 object Matrices {
-  def gameOfLife(board: Array[Array[Int]]): Unit = {
+  def gameOfLifeNaive(board: Array[Array[Int]]): Unit = {
     /** Naive solution.
      *
      * Space complexity: O(m x n)
@@ -25,4 +25,49 @@ object Matrices {
     for (ordinate <- 0 until n; absciss <- 0 until m) res(ordinate)(absciss) = nextState(absciss, ordinate)
     for (ordinate <- 0 until n; absciss <- 0 until m) board(ordinate)(absciss) = res(ordinate)(absciss)
   }
+
+  def gameOfLife(board: Array[Array[Int]]): Unit = {
+    /**
+     * An idea: while iterating, assigning new values, denoting both previous state and current state of the variable:
+     *
+     * prev_state -> current_state : denoting value
+     * 1 -> 0 : -1
+     * 0 -> 1 : 2
+     * 1 -> 1 : 1
+     * 0 -> 0 : 0
+     *
+     * Time complexity: O(m x n)
+     * Space complexity: O(1)
+     * */
+    val (m, n) = (board(0).length, board.length)
+
+    def countNeighbours(i: Int, j: Int): Int = {
+      (((i - 1) max 0) to ((i + 1) min (m - 1)))
+        .flatMap(absciss => (((j - 1) max 0) to ((j + 1) min (n - 1)))
+          .map(ordinate => (absciss, ordinate)))
+        .map {
+          case (absciss: Int, ordinate: Int) if board(ordinate)(absciss) == -1 => 1
+          case (absciss: Int, ordinate: Int) if board(ordinate)(absciss) == 2 => 0
+          case (absciss: Int, ordinate: Int) => board(ordinate)(absciss)
+        }.sum - board(j)(i)
+    }
+
+    def nextState(i: Int, j: Int): Int = {
+      (board(j)(i), countNeighbours(i, j)) match {
+        case (oldVal, neighbours) if oldVal == 1 && (neighbours < 2 || neighbours > 3) => -1
+        case (_, neighbours) if neighbours < 2 || neighbours > 3 => 0
+        case (oldVal, neighbours) if oldVal == 0 && neighbours == 3 => 2
+        case (_, neighbours) if neighbours == 3 => 1
+        case (oldVal, neighbours) if neighbours == 2 => oldVal
+      }
+    }
+
+    for (ordinate <- 0 until n; absciss <- 0 until m) board(ordinate)(absciss) = nextState(absciss, ordinate)
+
+    for (ordinate <- 0 until n; absciss <- 0 until m) {
+      if (board(ordinate)(absciss) == 2) board(ordinate)(absciss) = 1
+      else if (board(ordinate)(absciss) == -1) board(ordinate)(absciss) = 0
+    }
+  }
+
 }
--- src/test/scala/MatricesTest.scala
+++ src/test/scala/MatricesTest.scala
@@ -3,7 +3,20 @@ import org.scalatest.FunSuite
 import Matrices._
 
 class MatricesTest extends FunSuite {
-  test("Game of Life") {
+  test("Game of Life Naive") {
+    val conditions: Array[(Array[Array[Int]], Array[Array[Int]])] =
+      Array(
+        (Array(Array(0, 1, 0), Array(0, 0, 1), Array(1, 1, 1), Array(0, 0, 0)), Array(Array(0, 0, 0), Array(1, 0, 1), Array(0, 1, 1), Array(0, 1, 0))),
+        (Array(Array(1, 1), Array(1, 0)), Array(Array(1, 1), Array(1, 1)))
+      )
+
+    for (cond <- conditions) {
+      gameOfLifeNaive(cond._1)
+      cond._1.indices.foreach(i => assert(cond._1(i) sameElements cond._2(i)))
+    }
+  }
+
+  test("Game of Life - constant space") {
     val conditions: Array[(Array[Array[Int]], Array[Array[Int]])] =
       Array(
         (Array(Array(0, 1, 0), Array(0, 0, 1), Array(1, 1, 1), Array(0, 0, 0)), Array(Array(0, 0, 0), Array(1, 0, 1), Array(0, 1, 1), Array(0, 1, 0))),
@@ -15,4 +28,5 @@ class MatricesTest extends FunSuite {
       cond._1.indices.foreach(i => assert(cond._1(i) sameElements cond._2(i)))
     }
   }
+
 }
