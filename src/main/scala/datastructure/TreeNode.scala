f4851d3fff8f6b4827d2898ef6932662db701582
lastseenjustnow <lastseenjustnow@protonmail.com>
2022-04-20 17:18:13 +0400

    Auxiliary function to create a tree from an Array

--- src/main/scala/datastructure/TreeNode.scala
+++ src/main/scala/datastructure/TreeNode.scala
@@ -6,4 +6,21 @@ class TreeNode(_value: Int = 0,
   var value: Int = _value
   var left: TreeNode = _left
   var right: TreeNode = _right
+
+  def fromArray(arr: Array[Any]): TreeNode = {
+    val arrNodes = arr.map {
+      case null => null
+      case someVal: Int => new TreeNode(someVal)
+    }
+
+    for (i <- 1 until arr.length) {
+      val nodeIndex = (i - 1) / 2
+      val leftOrRight = (i - 1) % 2
+      leftOrRight match {
+        case x if x == 0 => arrNodes(nodeIndex).left = arrNodes(i)
+        case x if x == 1 => arrNodes(nodeIndex).right = arrNodes(i)
+      }
+    }
+    arrNodes(0)
+  }
 }
