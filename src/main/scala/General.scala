ef965463a0f5344feb5f73d617fa7ddecfb9e646
lastseenjustnow <lastseenjustnow@protonmail.com>
2022-05-21 20:00:05 +0400

    LeetCode: Maximum White Tiles Covered by a Carpet Not solved...

--- src/main/scala/General.scala
+++ src/main/scala/General.scala
@@ -1,3 +1,5 @@
+import SortingAndSearching.{searchInsert, searchInsertFirst}
+
 import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.math.{abs, max, min}
@@ -300,5 +302,25 @@ object General {
       .dropRight(1)
       .count(x => x._1 >= x._2)
   }
+
+//  def maximumWhiteTiles(tiles: Array[Array[Int]], carpetLen: Int): Int = {
+//    val n = tiles.length
+//    val ts = tiles.map(x => (x(0), x(1))).sorted
+//    val startPos = ts.map(_._1)
+//    val preSum = Array.fill(n + 1)(0)
+//    for (i <- 1 until n + 1) preSum(i) = preSum(i - 1) + (ts(i - 1)._2 - ts(i - 1)._1 + 1)
+//
+//    var res = 0
+//
+//    for (i <- 0 until n) {
+//      val tile = ts(i)
+//      if (tile._2 >= tile._1 + carpetLen - 1) return carpetLen
+//      val endIdx = searchInsertFirst(startPos, tile._1 + carpetLen - 1) min (n - 1)
+//      var compensate = 0
+//      if (ts(endIdx)._2 > tile._1 + carpetLen - 1) compensate = ts(endIdx)._2 - tile._1 - carpetLen + 1
+//      res = max(res, preSum(endIdx + 1) - preSum(i) - compensate)
+//    }
+//    res
+//  }
 
 }
